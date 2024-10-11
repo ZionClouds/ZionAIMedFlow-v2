bed06b66baa95ed4513f1601ac7d7ad6aa5cf089
Alex Morales <alex.morales@zionclouds.com>
2024-10-11 19:48:10 -0400

    Refactor database connection settings in settings.py

--- dpsiw/services/settings.py
+++ dpsiw/services/settings.py
@@ -16,12 +16,6 @@ class Settings:
         self.version = os.getenv("VERSION") or "2024-02-15-preview"
         self.speech_region = os.getenv("SPEECH_REGION")
         self.speech_key = os.getenv("SPEECH_API_KEY")
-        self.pg_host = os.getenv("PG_HOST")
-        self.pg_port = os.getenv("PG_PORT") or "5432"
-        self.pg_user = os.getenv("PG_USER") or "dbadmin"
-        self.pg_password = os.getenv("PG_PASSWORD")
-        self.pg_database = os.getenv("PG_DATABASE")
-        # self.pg_sslmode = os.getenv("PG_SSLMODE")
         self.mongo_conn_str = os.getenv("MONGO_DB")
         self.storage_url = os.getenv("STORAGE_URL")
         self.storage_key = os.getenv("STORAGE_KEY")
@@ -31,14 +25,13 @@ class Settings:
         if not bool(self.table_connection_string) or not bool(self.queue_connection_string) \
                 or not bool(self.blob_connection_string) or not bool(self.type) or not bool(self.chat_model) \
                 or not bool(self.endpoint) or not bool(self.api_key) or not bool(self.version) \
-                or not bool(self.speech_region) or not bool(self.speech_key) or not bool(self.pg_host) \
-                or not bool(self.pg_port) or not bool(self.pg_user) or not bool(self.pg_password) \
-                or not bool(self.pg_database) or not bool(self.mongo_conn_str) or not bool(self.storage_url) \
+                or not bool(self.speech_region) or not bool(self.speech_key) \
+                or not bool(self.mongo_conn_str) or not bool(self.storage_url) \
                 or not bool(self.storage_key):
             # os exist
-            logging.error("Some or all missing environment variables TABLE_CONNECTION_STRING, QUEUE_CONNECTION_STRING, STORAGE_CONNECTION_STRING, " +
-                          "TYPE, CHAT_MODEL, ENDPOINT, API_KEY, VERSION, SPEECH_REGION, SPEECH_API_KEY, PG_HOST, PG_PORT, PG_USER, PG_PASSWORD, " +
-                          "PG_DATABASE, MONGO_DB, STORAGE_URL, STORAGE_KEY")
+            logging.error("Missing environment variables including TABLE_CONNECTION_STRING, QUEUE_CONNECTION_STRING, " +
+                          "STORAGE_CONNECTION_STRING, TYPE, CHAT_MODEL, ENDPOINT, API_KEY, VERSION, SPEECH_REGION, " +
+                          "SPEECH_API_KEY, MONGO_DB, STORAGE_URL, STORAGE_KEY")
             exit(1)
 
 
