5bff0c883c52b52561528d5903a29c3be9d2aa37
Alex Morales <alex.morales@zionclouds.com>
2024-10-12 11:49:35 -0400

    Refactor settings.py to include Azure Service Bus connection string and queue name

--- dpsiw/services/settings.py
+++ dpsiw/services/settings.py
@@ -19,6 +19,8 @@ class Settings:
         self.mongo_conn_str = os.getenv("MONGO_DB")
         self.storage_url = os.getenv("STORAGE_URL")
         self.storage_key = os.getenv("STORAGE_KEY")
+        self.sb_connection_string = os.getenv("SB_CONNECTION_STRING")
+        self.sb_queue_name = os.getenv("SB_QUEUE") or "dips-messages"
 
         # print(f"Settings: {self.__dict__}")
 
@@ -27,11 +29,12 @@ class Settings:
                 or not bool(self.endpoint) or not bool(self.api_key) or not bool(self.version) \
                 or not bool(self.speech_region) or not bool(self.speech_key) \
                 or not bool(self.mongo_conn_str) or not bool(self.storage_url) \
-                or not bool(self.storage_key):
+                or not bool(self.storage_key) or not bool(self.sb_connection_string) \
+                or not bool(self.sb_queue_name):
             # os exist
             logging.error("Missing environment variables including TABLE_CONNECTION_STRING, QUEUE_CONNECTION_STRING, " +
                           "STORAGE_CONNECTION_STRING, TYPE, CHAT_MODEL, ENDPOINT, API_KEY, VERSION, SPEECH_REGION, " +
-                          "SPEECH_API_KEY, MONGO_DB, STORAGE_URL, STORAGE_KEY")
+                          "SPEECH_API_KEY, MONGO_DB, STORAGE_URL, STORAGE_KEY, SB_CONNECTION_STRING, SB_QUEUE")
             exit(1)
 
 
