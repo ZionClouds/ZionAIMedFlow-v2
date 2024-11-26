808097d97137a5585286d49a46ba2001aed293fe
Alex Morales <alex.morales@zionclouds.com>
2024-11-25 19:32:44 -0500

    Add support for asynchronous Azure OpenAI client and enhance LLMService

--- src/dpsiw/services/llmservice.py
+++ src/dpsiw/services/llmservice.py
@@ -1,6 +1,6 @@
 import logging
 
-from openai import OpenAI, AzureOpenAI
+from openai import OpenAI, AsyncOpenAI, AzureOpenAI, AsyncAzureOpenAI
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 from dpsiw.messages.message import LLMOpts
 from dpsiw.services.settingsservice import get_settings_instance
@@ -10,7 +10,7 @@ settings = get_settings_instance()
 aoaiclient = None
 
 
-def get_aoai_client_instance():
+def get_aoai_client_instance(is_async: bool = False):
     """
     Get the Azure OpenAI client instance
     """
@@ -19,26 +19,38 @@ def get_aoai_client_instance():
         token_provider = get_bearer_token_provider(
             DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
         )
-        if settings.is_dev:
-            aoaiclient = AzureOpenAI(azure_endpoint=settings.openai_endpoint,
-                                     azure_ad_token_provider=token_provider,
-                                     api_key=settings.openai_key,
-                                     api_version=settings.openai_version)
+        if is_async:
+            if settings.is_dev:
+                aoaiclient = AsyncAzureOpenAI(azure_endpoint=settings.openai_endpoint,
+                                              azure_ad_token_provider=token_provider,
+                                              api_key=settings.openai_key,
+                                              api_version=settings.openai_version)
+            else:
+                aoaiclient = AsyncAzureOpenAI(azure_endpoint=settings.openai_endpoint,
+                                              azure_ad_token_provider=token_provider,
+                                              # api_key=settings.api_key,
+                                              api_version=settings.openai_version)
         else:
-            aoaiclient = AzureOpenAI(azure_endpoint=settings.openai_endpoint,
-                                     azure_ad_token_provider=token_provider,
-                                     # api_key=settings.api_key,
-                                     api_version=settings.openai_version)
+            if settings.is_dev:
+                aoaiclient = AzureOpenAI(azure_endpoint=settings.openai_endpoint,
+                                         azure_ad_token_provider=token_provider,
+                                         api_key=settings.openai_key,
+                                         api_version=settings.openai_version)
+            else:
+                aoaiclient = AzureOpenAI(azure_endpoint=settings.openai_endpoint,
+                                         azure_ad_token_provider=token_provider,
+                                         # api_key=settings.api_key,
+                                         api_version=settings.openai_version)
     return aoaiclient
 
 
-class LLMService:
+class LLMService():
     """
     GPTTool class to interact with OpenAI API
     """
 
     def __init__(self, client) -> None:
-        self.client: OpenAI | AzureOpenAI | None = client
+        self.client: OpenAI | AzureOpenAI | AsyncAzureOpenAI | AsyncOpenAI | None = client
 
     def language_validator(self, content: str) -> str:
         """
@@ -72,6 +84,30 @@ class LLMService:
             messages = self.trimmer(messages, max_history=max_history)
 
         response = self.client.chat.completions.create(
+            model=opts.model,
+            messages=messages,
+            temperature=opts.temperature,
+            max_tokens=opts.max_tokens,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stream=opts.stream
+        )
+        # print(response)
+        return response.choices[0].message.content
+
+    async def completion_aio(self, opts: LLMOpts, messages: list[GPTMessage], trim: bool = False, max_history: int = 4) -> str:
+        """
+        Generate completion from the model
+        """
+        if messages is None or len(messages) == 0 or opts is None:
+            logging.error("No GPTMessage(s) or LLMOpts provided")
+            return ""
+
+        if trim:
+            messages = self.trimmer(messages, max_history=max_history)
+
+        response = await self.client.chat.completions.create(
             model=opts.model,
             messages=messages,
             temperature=opts.temperature,
